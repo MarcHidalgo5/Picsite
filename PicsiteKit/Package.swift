0a95b2acb86667abe184a100e5ca1b7be5fadb99
MarcHidalgo5 <90052996+MarcHidalgo5@users.noreply.github.com>
2022-02-11 17:39:23 +0100

    modify package

--- PicsiteKit/Package.swift
+++ PicsiteKit/Package.swift
@@ -14,9 +14,8 @@ let package = Package(
             targets: ["PicsiteKit"]),
     ],
     dependencies: [
-        .package(name: "Deferred",url: "https://github.com/theleftbit/Deferred.git", from: "4.2.1"),
         .package(name: "BSWFoundation", url: "https://github.com/theleftbit/BSWFoundation.git", from: "5.0.0"),
-        .package(name: "SnapshotTesting", url: "https://github.com/pointfreeco/swift-snapshot-testing.git", from: "1.8.2"),
+        .package(name: "SnapshotTesting", url: "https://github.com/pointfreeco/swift-snapshot-testing.git", from: "1.9.0"),
     ],
     targets: [
         .target(
@@ -27,8 +26,7 @@ let package = Package(
         .testTarget(
             name: "PicsiteKitTests",
             dependencies: ["PicsiteKit", "SnapshotTesting"],
-            path: "Tests",
-            exclude: ["__Snapshots__"]
+            path: "Tests"
         ),
     ]
 )
