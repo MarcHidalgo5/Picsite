fa98a9ae7f3ba60d21d12b018047bdb2cf23a5b8
MarcHidalgo5 <90052996+MarcHidalgo5@users.noreply.github.com>
2022-02-26 12:39:07 +0100

    ass dependencies in PicsiteUI package

--- PicsiteUI/Package.swift
+++ PicsiteUI/Package.swift
@@ -5,20 +5,27 @@ import PackageDescription
 
 let package = Package(
     name: "PicsiteUI",
+    platforms: [
+        .iOS(.v14),
+    ],
     products: [
         .library(
             name: "PicsiteUI",
-            targets: ["PicsiteUI"]),
+            targets: ["PicsiteUI"]
+        ),
     ],
     dependencies: [
+        .package(path: "PicsiteKit"),
+        .package(url: "https://github.com/JonasGessner/JGProgressHUD.git", from: "2.1.0"),
+        .package(url: "https://github.com/theleftbit/BSWFoundation.git", from: "5.0.0"),
+        .package(url: "https://github.com/theleftbit/BSWInterfaceKit.git", from: "5.0.0"),
     ],
     targets: [
         .target(
             name: "PicsiteUI",
             dependencies: [
-            ]),
-        .testTarget(
-            name: "PicsiteUITests",
-            dependencies: ["PicsiteUI"]),
+                
+            ]
+        ),
     ]
 )
